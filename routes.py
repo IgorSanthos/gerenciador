f073787bacc847854c0e32c83940ce869e40cff8
igorSanthos <igorsanthoss@gmail.com>
2024-06-17 14:05:31 -0300

    finalizado - falta colocar real arquivos - copiar e depois excluir

---
+++ routes.py
@@ -0,0 +1,90 @@
+
+from flask  import Flask, render_template, redirect, url_for
+from move_func import move_files
+import pandas as pd
+from bs4 import BeautifulSoup
+
+
+app = Flask(__name__)                   # Obrigatorio - Ativação do framework flask 
+#----------------------------------------   PAGINA 1 -  HOME
+@app.route('/')
+def index():        
+    return render_template('index.html')
+
+# ---------------------------------------------
+
+
+@app.route('/move', methods=['POST'])
+def move():
+
+    dtCliente = '04_2024'
+    with open('C:\\Users\\Igor\\Desktop\\Gerenciador\\templates\\index.html', 'r', encoding='utf-8') as file:
+        html_content = file.read()
+
+    soup = BeautifulSoup(html_content, 'html.parser')
+    table = soup.find('table')
+    rows = table.find_all('tr')
+    data = []
+    for row in rows:
+        cols = row.find_all('td')
+        if len(cols) >= 4:  # Verifica se há pelo menos duas colunas
+            datacluster = cols[0].text.strip()
+            nome = cols[1].text.strip()
+            origem = cols[2].text.strip()
+            destino = cols[3].text.strip()
+            data.append([datacluster, nome, origem, destino])
+    # Cria o DataFrame com a origem e destino selecionados
+    df_filtrado = pd.DataFrame(data, columns=['DataCluster', 'Nome', 'Origem', 'Destino'])
+
+    # Chamando a funcao Move
+    move_files(df_filtrado)
+    return redirect(url_for('index'))
+
+
+
+#------------------------------------------------------------------------------------------
+# #----------------------------------------  PAGINA 2 - SELEÇÃO DE CLIENTES
+@app.route('/selecionar-cliente',methods=['GET', 'POST'])                               # Route -> e o que vem depois do nome do site por se eu colocar (/) a homepage
+def clienteGeral():
+    return render_template("pagina_2.html")
+# #---------------------------------------------------------------------------------------
+# #---------------------------------------------- PAGINA 3 - ADICIONAR NOVO  
+@app.route('/adicionar-cliente')
+
+def adicionar():
+    return render_template("pagina_3.html")
+ 
+# #------------------------------------------------------------------------------------------
+# #-----------------------------------------------------------DELETE PAGINA 4
+
+@app.route('/excluir')
+def selectDelete():
+    return render_template("pagina_4.html")
+
+# #---------------------------------  
+
+# Obrigatorio - Atualiza a pagina 
+if __name__ == "__main__":              
+    app.run(debug=True)
+
+
+
+
+                    # Servidor do heroku
+                    
+# @app.route('/', methods=['POST', 'GET'])
+# def index():
+#     if request.method   ==  'POST':
+#         dataCluster = request.form.get('dayCluster')
+#         df = dataCluster
+#         df_filtrado = df[df['DataCluster'] == dataCluster]
+#         try:
+#             mv.move(df_filtrado)
+#             return ''''<script>
+#                     alert("Arquivos movidos com sucesso");
+#                     window.location.href = '/';
+#                     </script>''' 
+#         except Exception as e:
+#             print('Erro: ', e)
+
+#     return render_template('index.html')
